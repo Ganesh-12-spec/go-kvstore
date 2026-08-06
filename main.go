80901a205a69ff6c66c8cedc9dfc4fbffa11d34c
Ganesh Mhetre <mhetreg458@gmail.com>
2026-08-06 06:40:56 +0000

    kvstore: add POST /key endpoint to set values

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+
 	"net/http"
 )
 
@@ -13,6 +13,15 @@ type Entry struct {
 type Store struct {
 	data map[string]Entry
 }
+type SetRequest struct {
+	Key string `json:"key"`
+	Value string `json:"value"`
+}
+
+
+
+
+
 func NewStore() *Store {
 	return &Store{
 		data: make(map[string]Entry),
@@ -36,9 +45,7 @@ func (s *Store) Get(key string) (string, bool) {
 
 func main() {
   store := NewStore()
-	store.Set("name", "Ganesh")
-  val, ok := store.Get("name")
-	fmt.Println(val, ok)
+	
 
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -46,7 +53,18 @@ func main() {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(response)
 	})
+	http.HandleFunc("/keys", func(w http.ResponseWriter, r *http.Request) {
+		var req SetRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w,"invaid request", http.StatusBadRequest)
+			return
+		}
+      store.Set(req.Key,req.Value)
 
+			response := map[string]string{"status":"ok"}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+	})
 	http.ListenAndServe(":8080", nil)
 
 	
